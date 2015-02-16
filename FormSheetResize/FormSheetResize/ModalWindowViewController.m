a4622120bc04980527035c27ba74dcac3f9514d3
ingybing <github@ingybing.co.uk>
2015-02-16 17:27:54 +0000

    Update ModalWindowViewController.m
    Potential Solution 1
    
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    IMPORTANT NOTE
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    Storyboard must be set to use Auto Layout or it will get hung up with repeated calls to viewWillLayoutSubviews

--- FormSheetResize/FormSheetResize/ModalWindowViewController.m
+++ FormSheetResize/FormSheetResize/ModalWindowViewController.m
@@ -10,7 +10,7 @@
 
 @interface ModalWindowViewController ()
 @property bool minimised;
-@property bool isInitialViewLoadLayout;
+@property CGRect windowBounds;
 @end
 
 #define is_iOS8 ([[[UIDevice currentDevice] systemVersion]floatValue] >= 8)
@@ -19,56 +19,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 
+    // Start with a minimised window size.
     self.minimised = YES;
-    self.isInitialViewLoadLayout = YES;
-    
+    self.windowBounds = CGRectMake(0, 0, 300, 300);
+   
     if(is_iOS8)
     {
         // Set initial size in iOS 8 etc.
-        self.preferredContentSize = CGSizeMake(300, 300);
+        self.preferredContentSize = self.windowBounds.size;
     }
 }
 
 - (void) viewWillLayoutSubviews
 {
-    if (self.isInitialViewLoadLayout)
-    {
-        self.isInitialViewLoadLayout = NO;
-        
-        if(!is_iOS8)
-        {
-            // Set initial size in iOS 6,7 etc.
-            self.view.superview.bounds = CGRectMake(0, 0, 300, 300);
-        }
-    }
+    // Resize the window based on it's current required size.
+    // The supeview bounds only seems to actually changed on screen
+    // if changed inside this method.
+    self.view.superview.bounds = self.windowBounds;
 }
 
 - (IBAction)minMaxTouched:(id)sender
 {
-    CGRect newSize = CGRectMake(0, 0, 300, 300);
-    
+    // Toggle the dimensions of the window bases on minimised / maximised state.
     if (self.minimised == YES)
     {
-        newSize = CGRectMake(0, 0, 600, 600);
-    }
-    
-    if(is_iOS8)
-    {
-        // Doesn't Work
-        // self.view.superview.bounds = newSize;
-        
-        // Doesn't seem to do anything either.
-        self.preferredContentSize = newSize.size;
+        self.windowBounds = CGRectMake(0, 0, 600, 600);
     }
     else
     {
-        // Works a treat on ios7
-        self.view.superview.bounds = newSize;
+        self.windowBounds = CGRectMake(0, 0, 300, 300);
     }
     
+    // Set some value that will get overridden in viewWillLayoutSubviews
+    // You need to change the view bounds or it won't actually invoke a layout.
+    self.view.superview.bounds = CGRectMake(0, 0, 100, 100);
+    
+    // Toggle minimised state before we layout.
     self.minimised = !self.minimised;
+    
+    // Manually request a layout. Since the superview bounds have been changed
+    // to a temporary value it should cause a layout where we set the real
+    // desired size.
+    [self.view.superview layoutIfNeeded];
 }
 
 - (IBAction)closeTouched:(id)sender
